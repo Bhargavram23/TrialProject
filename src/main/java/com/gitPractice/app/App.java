2610c56446bccfa7e61fec74e784213fa42ab876
Bhargav Ram <rambhargav017@gmail.com>
2024-06-03 10:39:50 +0530

    initial upload of files

---
+++ src/main/java/com/gitPractice/app/App.java
@@ -0,0 +1,13 @@
+package com.gitPractice.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
